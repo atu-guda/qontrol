9f8a3c481a75e7fcaa2bbcb708704992637c09d1
Anton Guda <atu@nmetau.edu.ua>
2012-07-01 00:56:59 +0300

    Add PRM_* to TFourier.

--- elems/tfourier.h
+++ elems/tfourier.h
@@ -63,18 +63,39 @@ class TFourier : public TMiso  {
    void initVars(void);
  protected:
    /** period parameters  */
-   double tp, t0, t1, omega;
+   PRM_DOUBLE1( omega, efNoRunChange, "\\omega", "base frequency", "min=1e-3" );
+   PRM_DOUBLE1( t0, efNoRunChange, "t0", "Start time", "" );
+   PRM_DOUBLE1( t1, efNoRunChange, "t1", "Stop  time", "" );
+   PRM_INT1( ng, efNoRunChange, "N of Harm", "Number of harmonics to analize", "" );
+   PRM_SWITCH1( useOptim, efNoRunChange, "Optimize", "Optimize t1 to  better FT", "" );
+   PRM_SWITCH1( useFill, efNoRunChange, "Fill cmp", "Fill compare array:", "" );
+   PRM_INT1( np, efNoRunChange, "np", "np", "sep=col" );
+   PRM_INT1( nn, efNoRunChange, "nn", "nn", "" );
+   PRM_INT1( out_a, efNoRunChange, "out_a", "start index for a[i]", "min=0\nmax=1000" );
+   PRM_INT1( out_b, efNoRunChange, "out_b", "start index for a[i]", "min=0\nmax=1000" );
+   PRM_INT1( out_am, efNoRunChange, "out_am", "start index for A[i]", "min=0\nmax=1000" );
+   PRM_INT1( out_om, efNoRunChange, "out_om", "start index for omega[i]", "min=0\nmax=1000" );
+   /** name of array to and from fill */
+   PRM_STRING1( x_oname, efNoRunChange, "x[] in ", "in x[] array name", "sep=col" );
+   PRM_STRING1( y_oname, efNoRunChange, "y[] out", "out y[] array name", "" );
+   PRM_STRING1( a_oname, efNoRunChange, "a[] out", "output array for a[i]", "" );
+   PRM_STRING1( b_oname, efNoRunChange, "b[] out", "output array for b[i]", "" );
+   PRM_STRING1( am_oname, efNoRunChange, "A[] out", "output array for A[i]", "" );
+   PRM_STRING1( om_oname, efNoRunChange, "ome[] out", "output array for omega[i]", "" );
    /** output values */
-   double a0, a1, b1, ampl1, phi, ampl, qpow, qpow1, s_x2;
-   /** N of garm, flag for optim., integer representation of period values */
-   int ng, useOptim, useFill, nper, np, nn, n_st, n_en, 
-       ii, out_a, out_b, out_am, out_om;
+   PRM_DOUBLE1( tp, efInner, "period", "base period for given frequency", "" );
+   PRM_DOUBLE1( a0, efInner, "a0", "constant part", "" );
+   PRM_DOUBLE1( a1, efInner, "a1", "part at cos(omega*t)", "" );
+   PRM_DOUBLE1( b1, efInner, "b1", "part at sin(omega*t)", "" );
+   PRM_DOUBLE1( ampl1, efInner, "ampl1", "Amplitude of first harmonic", "" );
+   PRM_DOUBLE1( phi, efInner, "phi", "Phase of first harmonic", "" );
+   PRM_DOUBLE1( ampl, efInner, "ampl", "Amplitude of all calculated harmonics?", "" );
+   PRM_DOUBLE1( qpow, efInner, "qpow", "power criterion?", "" );
+   PRM_DOUBLE1( qpow1, efInner, "qpow1", "power criterion1?", "" );
+   PRM_DOUBLE1( s_x2, efInner, "s_x2", "Input power", "" );
+   int nper, n_st, n_en, ii;
    /** arrays for summators */
    double *aa, *bb, *am;
-   /** name of array to and from fill */
-   QString x_oname, y_oname, 
-        a_oname, b_oname, 
-	am_oname, om_oname;
    /** data descriptors -- with base class elements */ 
    static TDataInfo tfourier_d_i[50];
    /** class decription */
