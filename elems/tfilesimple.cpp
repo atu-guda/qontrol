8ee5ea0ead49cf6f204587e9c6bb12f587a62d66
Anton Guda <atu@nmetau.edu.ua>
2016-01-26 12:54:58 +0200

    Fix TFileSimple preRead and counters.

--- elems/tfilesimple.cpp
+++ elems/tfilesimple.cpp
@@ -29,10 +29,24 @@ CTOR(TFileSimple,TMiso)
 
 double TFileSimple::f() noexcept
 {
+  ++hold_i;
+  if( hold_i < hold_n ) {
+    return v[0];
+  }
+  hold_i = 0;
+
   if( useDef ) {
     v.val().assign( (unsigned)(rnc), (double)(defVal) );
   }
 
+  readLine();
+
+  return v[0];
+}
+
+int TFileSimple::readLine() noexcept
+{
+  int nread_col = 0;
   while( ! file.atEnd() ) { // EOF may be temporary, main exit - by break
     lin = file.readLine( buf_sz ).simplified();
     if( lin.size() < 1 || lin[0] == '#' || lin[0] == ';' ) { // empty or comment
@@ -41,21 +55,26 @@ double TFileSimple::f() noexcept
 
     QList<QByteArray> lba = lin.split( sep_c );
     // qWarning() << "Split: [" << lba.size() << "] " << lba << " c=" << (int)sep_c <<  NWHE;
-    int i = 0, nread = 0;
+    int i = 0;
     for( auto s : lba ) {
       bool ok;
       double a = s.toDouble( &ok );
       if( ok ) {
-        v[i] = a * scale; ++nread;
+        v[i] = a * scale; ++nread_col;
       }
       ++i;
     }
-    if( nread > 0 ) {
-      break;
+    if( nread_col > 0 ) {
+       ++n_total; hold_i = 0;
+       break;
     }
   };
 
-  return v[0];
+  // skip every_n-1 lines
+  for( int i=1; i<every_n; ++i ) { // 1: sic, one line is read beforehand
+    file.readLine( buf_sz );
+  }
+  return nread_col;
 }
 
 
@@ -79,15 +98,22 @@ int TFileSimple::do_postRun( int /*good*/ )
 
 int TFileSimple::miso_startLoop( int /*acnx*/, int /*acny*/ )
 {
+  n_total = 0; hold_i = 0;
   if( ! file.open( QIODevice::ReadOnly | QIODevice::Text ) ) {
     qWarning() << "Fail to open data file " << file.fileName() << NWHE;
     return 0;
   }
   v.val().assign( max_col, out0_init );
 
-  for( int i=0; i<skipLines; ++i ) {
+  for( int i=0; i<skipBefore; ++i ) {
     lin = file.readLine( buf_sz );
   }
+
+  if( !noCalcStart ) {
+    readLine(); out0 = v[0];
+    hold_i = 0; // to force ignore read
+  }
+
   return 1;
 }
 
--- elems/tfilesimple.h
+++ elems/tfilesimple.h
@@ -42,14 +42,21 @@ class TFileSimple : public TMiso  {
    virtual int miso_startLoop( int acnx, int acny ) override;
    virtual int do_endLoop() override;
 
+   int readLine() noexcept;
+
    PRM_STRING( filename, efNRC, "File name", "Input file name", "" );
    PRM_STRING( sep,  efNRC, "Separator", "Input field separator", "def= " );
    PRM_INT( rnc,  efNRC, "N of columns", "Number of columns to read", "min=1\nmax=32\ndef=1" );
-   PRM_INT( skipLines,  efNRC, "Skip lines", "Number of lines to skip", "min=0\ndef=0" );
+
+   PRM_INT( skipBefore,  efNRC, "Skip before", "Number of lines to skip before run", "min=0\ndef=0\nsep=col" );
+   PRM_INT( every_n,  efNRC, "Every N", "Skip (n-1) lines after converted first", "min=1\ndef=1" );
+   PRM_INT( hold_n,   efNRC, "Hold  N", "Hold converted line for N steps", "min=1\ndef=1" );
 
    PRM_PARAMD( scale, 0, "scale", "common coefficient for all outputs", "def=1.0\nsep=col" );
    PRM_PARAMD( defVal, 0, "Default", "Default value for EOF and EOL case", "def=0.0" );
    PRM_SWITCH( useDef, efNRC, "Use default", "Use default value, old value - elsewhere", "def=0" );
+
+   PRM_INT( n_total,  efInner, "n_total", "Total number of read and converted lines", "" );
 
    PRM_DOUBLE_ARR( v, efInner, "v", "Current data", "N=32\ndef=0.0" ); // 32 is max_col
 
@@ -58,6 +65,7 @@ class TFileSimple : public TMiso  {
    QFile file;
    QByteArray lin;
    char sep_c { ' ' };
+   int hold_i { 0 };
 
    Q_CLASSINFO( "nameHintBase",  "file_" );
    DCL_DEFAULT_STATIC;
