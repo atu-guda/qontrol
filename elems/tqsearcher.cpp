f0c53871c001fd531c909c3f8aefa737b74d8118
Anton Guda <atu@nmetau.edu.ua>
2017-01-06 20:35:11 +0200

    Better TQSearcher handling

--- elems/tqsearcher.cpp
+++ elems/tqsearcher.cpp
@@ -40,23 +40,87 @@ int TQSearcher::miso_startLoop( long /*acnx*/, long /*acny*/ )
 double TQSearcher::f() noexcept
 {
   // fallback values
-  p_e = out0;
   f_c = 0; f_n = 0; f_e = 0;
   double p_ct0 = out0 - out0_init;
+  pr_l = p_l - out0; pr_r = p_r - out0;
+  qr_l = q_l - q_o;  qr_c = q_c - q_o; qr_r = q_r - q_o; // not full: need /
+  p_e = out0; pr_e = 0.0; F_c = 0.0; S_e = 0.0;
 
-  double qr_c = q_o - q_c;
-  double q_r = qr_c / q_gamma;
-  double aq_r = fabs( q_r );
-  double q_r2 = q_r * q_r;
+  double q_rel_a = fabs( qr_c / q_gamma );
+  double q_rel2 = q_rel_a * q_rel_a;
 
   switch( (int)F_type ) {
-    case qa_Gauss  : F_c = exp( - q_r2 ); break;
-    case qa_Para   : F_c = 1.0 - q_r2 * oneMinusEm1; break;
-    case qa_Lin    : F_c = 1.0 - aq_r * oneMinusEm1; break;
-    case qa_Hyper  : F_c = 1.0 / ( 1.0 + aq_r * eMinus1 ); break;
-    case qa_Log    : F_c = 1.0 - log( 1.0 + aq_r ) * logQaScale; break;
+    case qa_Gauss  : F_c = exp( - q_rel2 ); break;
+    case qa_Para   : F_c = 1.0 - q_rel2 * oneMinusEm1; break;
+    case qa_Lin    : F_c = 1.0 - q_rel_a * oneMinusEm1; break;
+    case qa_Hyper  : F_c = 1.0 / ( 1.0 + q_rel_a * eMinus1 ); break;
+    case qa_Log    : F_c = 1.0 - log( 1.0 + q_rel_a ) * logQaScale; break;
     default        : F_c = 0.0; break;
   };
+  if( limitF ) {
+    F_c = vBound( 0.0, F_c, 1.0 );
+  }
+
+  do { // calclucate p_e, S_e with local exit via break
+    if( F_c > 0.999999 ) { // precise
+      S_e = 1.0;
+      break;
+    }
+    qr_l /= qr_c; qr_r /= qr_c;
+    if( pr_l > -1e-12 || pr_r < 1e-12 ) { // BEWARE: dimension!
+      break;
+    }
+
+    double pr_el = pr_l / ( 1.0 - qr_l ); // zero?
+    double pr_er = pr_r / ( 1.0 - qr_r );
+
+    if( qr_l > 0 && qr_r > 0 ) {  // ----------- one side from q_o ----------------
+      if( qr_l > 1.0 ) {
+        if( qr_r > 1.0 ) {    // :.: central point is better, but not good
+          pr_e = 0.1 * pr_l + 0.1 * pr_e; S_e = exp( -( pr_er*pr_er ) / (pr_r*pr_l) ); // C_cg
+          S_e = 0.7;
+        } else {            // ::.
+          pr_e = 0.99 * pr_r; S_e = exp( -( pr_er*pr_er ) / (pr_r*pr_r) ); // Cr
+        }
+      } else {
+        if( qr_r > 1 ) {    // .::
+          pr_e = 0.99 * pr_l;  S_e = exp( -( pr_el*pr_el ) / (pr_l*pr_l) ); // Cl
+        } else {            // .:.
+          double pr_x = ( qr_l < qr_r ) ? pr_l : pr_r; // C_cb
+          S_e = exp( -( pr_el*pr_er ) / (pr_l*pr_r) );
+          pr_e = 0.99 * pr_x;
+        }
+      }
+      break;
+    }
+
+    if( qr_l < 0 && qr_r < 0 ) { // ------------- double cross ---------------
+      // mix?
+      S_e = 0.5;
+      break;
+    }
+
+    if( qr_l < 0  ) { // 2L
+      pr_e = pr_l / ( 1 - qr_l );  S_e = exp( -( pr_e*pr_e ) / (pr_l*pr_l) );
+    } else {          // 2R
+      pr_e = pr_r / ( 1 - qr_r );  S_e = exp( -( pr_e*pr_e ) / (pr_r*pr_r) );
+    }
+    break;
+  } while( false );
+  p_e = pr_e + out0;
+  FS_e = F_c * S_e;
+
+
+  switch( (FeType)(int)(fe_type) ) {
+    case fe_lin:
+       f_e = S_e * k_e * ( p_e - out0 ); break;
+    case fe_sign:
+       f_e = S_e * k_e * sign( p_e - out0 ); break;
+    case fe_lim:
+       f_e = S_e * k_e * limitLine( p_e - out0, ( p_max - p_min ) * lin_rlim ); break;
+    default:
+       f_e = 0;
+  }
 
   f_c = - k_cl  * ( p_ct0 )
         - k_ch  * barrierHypUp(    out0, p_max )
@@ -65,7 +129,6 @@ double TQSearcher::f() noexcept
         - k_nh  * barrierHypUp(    out0, p_r )
         + k_nh  * barrierHypDown(  out0, p_l );
 
-  f_e *= k_e;
 
   f_t = f_c + f_n + f_e;
   double p_cn = out0 + (double)f_t * v_f * ctdt;
--- elems/tqsearcher.h
+++ elems/tqsearcher.h
@@ -30,6 +30,14 @@ class TQSearcher : public TMiso  {
    DCL_CREATE;
    DCL_STD_INF;
 
+   enum FeType {
+     fe_lin = 0, fe_sign, fe_lim
+   };
+   Q_ENUMS(FeType);
+   Q_CLASSINFO( "enum_FeType_0",  "Linear"       );    // fe_lin
+   Q_CLASSINFO( "enum_FeType_1",  "Sign"         );    // fe_sign
+   Q_CLASSINFO( "enum_FeType_2",  "Limit"        );    // fe_lim
+
    enum QualType { // like TQuality
      qa_Gauss = 0, qa_Para, qa_Lin, qa_Hyper, qa_Log
    };
@@ -49,26 +57,29 @@ class TQSearcher : public TMiso  {
    virtual int miso_startLoop( long acnx, long acny ) override;
 
    // f_e
-   PRM_PARAMD( v_f,     0,   "&v_f",     "Velocity factor", "def=1\nsep=block" );
-   PRM_PARAMD( k_e,     0,   "k_{&e}",   "f_e factor", "def=2.0\nsep=col" );
+   PRM_PARAMD( v_f     , 0 , "&v_f"        , "Velocity factor"        , "def=1\nsep=block" );
+   PRM_LIST( fe_type   , 0 , "f_e type"    , "f_e calculation rule"   , "enum=FeType\nsep=col" );
+   PRM_PARAMD( k_e     , 0 , "k_{&e}"      , "f_e factor"             , "def=2.0\nsep=col" );
+   PRM_PARAMD( lin_rlim, 0 , "lin_{rlim}"  , "Linear f_e limit scale" , "def=0.1\nsep=col" );
    // f_c
-   PRM_PARAMD( k_cl,    0,   "k_{&cl}", "Linear f_c factor",      "def=0.5\nsep=block" );
-   PRM_PARAMD( k_ch,    0,   "k_{ch}",  "Hyperbolic f_c factor",  "def=0.0\nsep=col" );
-   PRM_PARAMD( p_min,   0,   "p_{min}", "Low p limit (for f_c)",  "def=-1000.0\nsep=col" );
-   PRM_PARAMD( p_max,   0,   "p_{max}", "High p limit (for f_c)", "def=1000.0\nsep=col" );
+   PRM_PARAMD( k_cl    , 0 , "k_{&cl}"     , "Linear f_c factor"      , "def=0.5\nsep=block" );
+   PRM_PARAMD( k_ch    , 0 , "k_{ch}"      , "Hyperbolic f_c factor"  , "def=0.0\nsep=col" );
+   PRM_PARAMD( p_min   , 0 , "p_{min}"     , "Low p limit (for f_c)"  , "def=-1000.0\nsep=col" );
+   PRM_PARAMD( p_max   , 0 , "p_{max}"     , "High p limit (for f_c)" , "def=1000.0\nsep=col" );
    // f_n
-   PRM_PARAMD( k_nl,    0,   "k_{&nl}",  "Linear f_n factor",     "def=1.0\nsep=block" );
-   PRM_PARAMD( k_nh,    0,   "k_{nh}",   "Hyperbolic f_n factor", "def=0.0\nsep=col" );
+   PRM_PARAMD( k_nl    , 0 , "k_{&nl}"     , "Linear f_n factor"      , "def=1.0\nsep=block" );
+   PRM_PARAMD( k_nh    , 0 , "k_{nh}"      , "Hyperbolic f_n factor"  , "def=0.0\nsep=col" );
    // F
-   PRM_LIST( F_type,    0,    "F t&ype",     "Quality type", "enum=QualType\nsep=block" );
-   PRM_PARAMD( q_gamma, 0,    "q_{\\gamma}", "f_e factor", "def=2.0\nsep=col" );
-   PRM_SWITCH( limitF,  0,    "Limit F", "Limit F to [0;1]", "def=1\nsep=col" );
+   PRM_LIST( F_type    , 0 , "F type"      , "Quality type"           , "enum=QualType\nsep=block" );
+   PRM_PARAMD( q_gamma , 0 , "q_{\\gamma}" , "f_e factor"             , "def=2.0\nsep=col" );
+   PRM_SWITCH( limitF  , 0 , "Limit F"     , "Limit F to [0;1]"       , "def=1\nsep=col" );
 
    // Outputs
    PRM_DOUBLE( p_e,   efInner, "p_e", "x of calculated extremum point", "" );
    PRM_DOUBLE( pr_e,  efInner, "pr_e", "Relative p of calculated extremum point", "" );
    PRM_DOUBLE( F_c,   efInner, "F_c", "F of central point", "" );
    PRM_DOUBLE( S_e,   efInner, "S_e", "Sure level for extremum point", "" );
+   PRM_DOUBLE( FS_e,  efInner, "FS_e", "F_C * S_e", "" );
    // forces
    PRM_DOUBLE( f_c,   efInner, "f_c",    "force to initial parameter value", "" );
    PRM_DOUBLE( f_n,   efInner, "f_n",    "force to neigbours", "" );
@@ -77,8 +88,9 @@ class TQSearcher : public TMiso  {
    // aux
    PRM_DOUBLE( pr_l,  efInner, "pr_l", "Relative x left point = p_l-p_c", "" );
    PRM_DOUBLE( pr_r,  efInner, "pr_r", "Relative x right point = p_r-p_c", "" );
-   PRM_DOUBLE( qr_l,  efInner, "qr_l", "Relative y left point = q_l-q_c", "" );
-   PRM_DOUBLE( qr_r,  efInner, "qr_r", "Relative y right point = q_r-q_c", "" );
+   PRM_DOUBLE( qr_l,  efInner, "qr_l", "Relative y left point = (q_l-q_p)/q_c", "" );
+   PRM_DOUBLE( qr_c,  efInner, "qr_l", "Relative y left point = (q_l-q_p)/q_c", "" );
+   PRM_DOUBLE( qr_r,  efInner, "qr_r", "Relative y right point = (q_r-q_o)/q_c", "" );
 
    PRM_INPUT( p_l,   0, "p_{&l}", "Left point parameter",  "sep=block" );
    PRM_INPUT( q_l,   0, "&q_l",   "Left point q",  "" );
